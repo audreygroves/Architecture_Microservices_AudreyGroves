c91f13164a1dca91aabbd546f1dc390247f905e9
audreygroves <145497739+audreygroves@users.noreply.github.com>
2023-11-02 15:09:17 -0400

    update

--- libraryApp/express-locallibrary-tutorial/app.js
+++ libraryApp/express-locallibrary-tutorial/app.js
@@ -9,6 +9,17 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//set up mongoose connection 
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = "mongodb+srv://ahgroves99:Pinkgirl14@cluster0.s12gbdc.mongodb.net/local_library?retryWrites=true&w=majority"
+
+mainModule().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
